d9a788ca2d432b06edf11203f16e587bbee78e0b
MAC-Anas <riahianas.ma@gmail.com>
2020-06-25 17:42:46 +0100

    fix: mock service url

--- app/src/main/java/com/example/emailcloneapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/emailcloneapp/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.example.emailcloneapp.R
 
 class HomeFragment : Fragment() {
@@ -20,12 +20,12 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = rootView.findViewById(R.id.text_home)
+        homeViewModel.emailData.observe(viewLifecycleOwner, Observer {
+            textView.text = it.toString()
         })
-        return root
+        return rootView
     }
 }
--- app/src/main/java/com/example/emailcloneapp/ui/home/data/EmailService.kt
+++ app/src/main/java/com/example/emailcloneapp/ui/home/data/EmailService.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface EmailService {
-    @GET
+    @GET(".")
     suspend fun getEmailData(): Response<List<EmailData>>
 }
